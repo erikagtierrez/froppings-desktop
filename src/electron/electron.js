81b7df33e0fcbca34690c5e0c8ae422b2e6e677a
Erika Gutierrez <erikagutierrez01@gmail.com>
2016-12-22 02:10:39 -0400

    Design :heart: Home getting shape and electron min  width and height set

--- src/electron/electron.js
+++ src/electron/electron.js
@@ -7,7 +7,7 @@ let win
  
 function createWindow () {
   // Create the browser window.
-  win = new BrowserWindow({width: 800, height: 600})
+  win = new BrowserWindow({width:1100, height:700, 'minHeight': 700,'minWidth': 1100})
  
   // and load the index.html of the app.
   win.loadURL(`file://${__dirname}/index.html`)
